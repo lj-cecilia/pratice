8d0459cdfa39c1eca0c338526c950e4862f219eb
lj-cecilia <123518070+lj-cecilia@users.noreply.github.com>
2023-09-28 17:37:35 -0400

    Add files via upload

---
+++ BSTIterator.java
@@ -0,0 +1,61 @@
+//import javax.swing.tree.TreeNode;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class BSTIterator {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    TreeNode node;
+    Stack<TreeNode> stack;
+    public BSTIterator(TreeNode root) {
+        node = root;
+        stack = new Stack<>();
+    }
+
+    public int next() {
+        while (node != null) {
+            stack.push(node);
+            node = node.left;
+        }
+        node = stack.pop();
+        int result = node.val;
+        node = node.right;
+        return result;
+    }
+
+    public boolean hasNext() {
+        return !stack.isEmpty();
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.next();
+ * boolean param_2 = obj.hasNext();
+ */
